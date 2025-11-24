45baaee5a9c2ba5b6e4aadf9e27bb63b5c8ead6a
ISHOOO <anantgupta08460@gmail.com>
2025-11-24 18:53:36 +0530

    Updated resume

--- homepage.py
+++ homepage.py
@@ -3,10 +3,7 @@ from PIL import Image, ImageEnhance
 from base64 import b64encode
 from io import BytesIO
 from streamlit_option_menu import option_menu
-import os
 
-BASE_DIR = os.path.dirname(__file__)
-RESUME_PATH = os.path.join(BASE_DIR, "static", "Resume.pdf")
 
 def jpg_to_base64(path):
     img = Image.open(path)
@@ -25,7 +22,7 @@ with open("styles.css") as f: st.markdown(f"<style>{f.read()}</style>", unsafe_a
 
 st.title(":green-background[ANANT GUPTA]")
 st.write("A Data Science and Analytics enthusiast looking for opportunities to solve real-world business problems by leveraging programming skills for :orange-background[Statistical Analysis], :orange-background[Graphical Visualization], :orange-background[Database management] and :orange-background[Predictive Modelling]. Committed to upskilling and providing actionable insights for data-driven decision making to foster innovation and efficiency.")
-with open(RESUME_PATH, "rb") as f: pdf_data = f.read()
+with open("Resume.pdf", "rb") as f: pdf_data = f.read()
 mail, resume, download,_ = st.columns([1.2,2,2.4,5]) 
 with mail: st.link_button(label="Hire Me", url="https://mail.google.com/mail/u/1/#inbox?compose=CllgCJNvwPFQXdsrTTFwqVQjFZtcsVMswwnsZxsCDnsWgxHxcDNgFpgQlWDTqznrFqDLmnsGGVB") 
 with resume: st.link_button(label="Preview Resume", url= "https://drive.google.com/file/d/18ueGxvXJZW0wv8hNYWsttRajy9-FVrKx/view?usp=sharing")
